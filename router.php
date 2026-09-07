25b95baf4fedeb7512de2bf12f76730a3693018e
Carolina Otermin <otermincarolina98@gmail.com>
2026-09-06 22:58:42 -0300

    Idem

--- router.php
+++
@@ -1,8 +0,0 @@
-<?php
-$file = $_SERVER["DOCUMENT_ROOT"] . $_SERVER["SCRIPT_NAME"];
-
-if (file_exists($file) && !is_dir($file)) {
-    return false; 
-} else {
-    include_once __DIR__ . '/mi404.php';
-}
